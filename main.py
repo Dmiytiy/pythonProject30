1ada9171eca74ae1ce958884ff0c2e429c2edcd2
Dmiytiy <prostoy.general@gmail.com>
2023-04-25 19:21:01 +0300

    q

--- main.py
+++ main.py
@@ -1,60 +1,49 @@
-#Установить aiogram и requests библиотеки для работы с Telegram API и публичными API.
-pip install aiogram requests
-#Получить токен для доступа к Telegram Bot API, зарегистрировав нового бота через @BotFather.
-#Создать модуль bot.py для реализации бота и подключить необходимые модули.
-import os
-import aiohttp
-from forex_python.converter import CurrencyRates
-import random
-from aiogram import Bot, Dispatcher, types
-from aiogram.types import ParseMode, InputMediaPhoto
-from aiogram.utils import executor
-import random
-#Загрузить переменные окружения из файла .env.
-TOKEN = os.environ.get('BOT_TOKEN') # токен доступа к боту
-#Создать экземпляр бота и диспетчера для обработки входящих сообщений и команд.
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
-#Создать функцию приветствия пользователя и предложения выбрать определенную функцию бота
-@dp.message_handler(commands=['start'])
-async def start(message: types.Message):
-    """
-    Отправляет сообщение со списком доступных команд.
-    """
-    await message.reply("Привет! Я могу помочь вам с погодой, валютой, картинками и опросами. "
-                        "Выберите одну из доступных команд: /weather, /currency, /picture, /poll")
+from django.forms.models import model_to_dict
+#1) В этом примере мы получаем текущего пользователя из контекста запроса (self.context['request'].user) и
+# присваиваем его в validated_data перед созданием объекта Entity с помощью метода create() модели Entity.
+class EntitySerializer(ModelSerializer):
+    value = IntegerField(...)
+    properties = PropertySerializer(many=True, read_only=True)
 
-#Создать функцию определения текущей погоды в определенном городе с использованием публичного API погоды и выдачи пользователю соответствующей информации.
-async def get_weather(message: types.Message):
-    """
-    Получает текущую погоду в указанном городе с помощью API OpenWeatherMap и отправляет результат пользователю.
-    """
-    city = message.text.split()[1]
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid=your_api_key&units=metric'
+    def create(self, validated_data):
+        user = self.context['request'].user
+        entity = Entity.objects.create(modified_by=user, **validated_data)
+        return entity
+#2)В этом примере мы указываем, что значение для поля value нужно брать из поля 'data[value]'
+# входного JSON (с помощью параметра source='data[value]').
 
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
-            data = await resp.json()
+#Далее мы в методе create() явно указываем, что нужно сохранять значение из поля 'data[value]'
+# в поле value модели Entity при создании объекта.
+class EntitySerializer(ModelSerializer):
+    value = IntegerField(source='data[value]')
+    properties = PropertySerializer(many=True, read_only=True)
 
-    if data['cod'] != 200:
-        await message.reply("Я не могу найти такой город. Пожалуйста, попробуйте снова.")
-    else:
-        temp = data['main']['temp']
-        feels_like = data['main']['feels_like']
-        description = data['weather'][0]['description']
-        await message.reply(f"Текущая температура в городе {city}: {temp}°C, ощущается как {feels_like}°C.\n{description.capitalize()}")
+    def create(self, validated_data):
+        user = self.context['request'].user
+        validated_data['modified_by'] = user
+        entity = Entity.objects.create(value=validated_data['data']['value'], modified_by=user)
+        return entity
 
-    # Обработчик команды /currency
-    @dp.message_handler(commands=['currency'])
-    async def convert_currency(message: types.Message):
-        """
-        Конвертирует указанную сумму из одной валюты в другую с помощью API Exchange Rates и отправляет результат пользователю.
-        """
-        try:
-            amount, from_currency, to_currency = message.text.split()[1:]
-            amount = float(amount)
-        except ValueError:
-            await message.reply("Неверный формат команды. Пожалуйста, введите сумму и валюты через пробел.")
-            return
 
-        c = Currency
+#В этом примере мы добавили поле properties с помощью SerializerMethodField(), чтобы определить свой метод get_properties() для его заполнения.
+
+#В методе get_properties() мы создаем пустой словарь properties и обходим все свойства объекта Entity,
+# преобразуя их в словарь с помощью метода model_to_dict(). Затем мы добавляем эти словари в общий словарь
+# properties,
+#где ключами будут значения поля key из модели Property, а значениями - значения поля value.
+class EntitySerializer(ModelSerializer):
+    value = IntegerField(source='data[value]')
+    properties = SerializerMethodField()
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        validated_data['modified_by'] = user
+        entity = Entity.objects.create(value=validated_data['data']['value'], modified_by=user)
+        return entity
+
+    def get_properties(self, obj):
+        properties = {}
+        for prop in obj.properties.all():
+            prop_dict = {'key': prop.key, 'value': prop.value}
+            properties[prop_dict['key']] = prop_dict['value']
+        return properties
